d18e5255afae10514e79ff1157ad92d3b249ed4d
arnaud2G <arnaud.verrier@gmail.com>
2017-05-15 19:26:49 +0200

    Correction archi

---
+++ AVForW/View/Weekend/WeekendCell.swift
@@ -0,0 +1,42 @@
+//
+//  WeekendCell.swift
+//  AVForW
+//
+//  Created by 2Gather Arnaud Verrier on 15/05/2017.
+//  Copyright © 2017 Arnaud Verrier. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import UIKit
+
+// MARK: - Cellule weekend
+class WeekendCell:UITableViewCell {
+    
+    @IBOutlet weak var lblThem: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var imgBack: UIImageView!
+    
+    var disposeBag:DisposeBag?
+    
+    var weekend:Weekend! {
+        didSet{
+            lblThem.text = weekend.printTopTheme()
+            lblTitle.text = weekend.label
+            
+            disposeBag = DisposeBag()
+            
+            // On observe l'image, si elle n'existe pas on lance le téléchargement
+            weekend.image.asObservable().subscribe(onNext:{
+                [weak self] image in
+                DispatchQueue.main.async(execute: {
+                    () -> Void in
+                    self?.imgBack.image = image
+                })
+                if image == nil {
+                    self?.weekend.dlImage()
+                }
+            }).addDisposableTo(disposeBag!)
+        }
+    }
+}
